00cd93dce7da6949e7ef3c8f11d31992168fb949
Wade-Coplen <wadecoplen@gmail.com>
2020-09-26 10:23:50 -0700

    1st commit - stuck with 3 errors on ring buffer. Moving on for now to names

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,22 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = 5
+        self.data = []
+        
+    class Full:
+        def append(self, item):
+            
+            self.data[self.cur] = item
+            self.cur = (self.cur + 1) % self.max
+            
+            
 
+        def get(self):
+            return self.data[self.cur:] + self.data[:self.cur]
     def append(self, item):
-        pass
-
+        self.data.append(item)
+        if len(self.data) == self.capacity:
+            self.cur = 0
+            
     def get(self):
-        pass
+        return self.data
